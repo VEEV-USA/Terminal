93433bfa48637be160bb11eea44fed2bf4ce828e
Hadeez <psychic1717@gmail.com>
2022-02-11 12:04:19 -0800

    instatiate coredata persistent store

--- chameleon/chameleon/Persistence.swift
+++ chameleon/chameleon/Persistence.swift
@@ -5,4 +5,33 @@
 //  Created by Dare on 2/7/22.
 //
 
+import CoreData
 import Foundation
+
+class Persistence: ObservableObject {
+    let container = NSPersistentContainer(name: "VModel")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data error: \(error.localizedDescription)")
+            }
+        }
+    }
+}
+
+extension Persistence {
+    class Users {
+        static func persist(userData: Dictionary<String, Any>, completion: @escaping () -> Void ) {
+            //MARK: TODO
+        }
+    }
+}
+
+extension Persistence {
+    class Checkins {
+        static func persist(checkinData: Dictionary<String, Any>, completion: @escaping () -> Void ) {
+            //MARK: TODO
+        }
+    }
+}
