af804160329e4ea9ceb1a2f3527c38b11d231056
Hadeez <psychic1717@gmail.com>
2022-02-16 01:19:00 -0800

    add auth service

---
+++ chameleon/chameleon/AuthService.swift
@@ -0,0 +1,12 @@
+//
+//  AuthService.swift
+//  chameleon
+//
+//  Created by Dare on 2/16/22.
+//
+
+import SwiftUI
+
+class AuthService: ObservableObject {
+    @Published var isAuth: Bool = false
+}
