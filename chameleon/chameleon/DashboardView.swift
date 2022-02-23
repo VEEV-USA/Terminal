fc0ce9c44302b68b7474ef5f4aa7dc7c42646215
Hadeez <psychic1717@gmail.com>
2022-02-22 18:02:58 -0800

    show checked in user info

--- chameleon/chameleon/DashboardView.swift
+++ chameleon/chameleon/DashboardView.swift
@@ -6,103 +6,34 @@
 //
 
 import SwiftUI
-import CoreData
-
-struct CheckinModel: Decodable, Identifiable {
-    let id = UUID()
-    let username: String
-    let date: Date
-    let time: String
-}
-
-protocol CheckinDelegate {
-    func checkin(_ checkin: Checkin)
-}
-
-struct CheckinsListView: View {
-    @ObservedObject var checkinsViewModel = CheckinsViewModel()
-    
-    var body: some View {
-        VStack {
-            HStack() {
-                Text("User")
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                Spacer(minLength: 30)
-                Text("Time")
-                    .fontWeight(.semibold)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            .padding(.bottom, 16)
-            ScrollView {
-                ForEach(checkinsViewModel.checkins) { checkin in
-                    HStack {
-                        Text(checkin.userHandle ?? "n/a")
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Spacer(minLength: 30)
-                        Text(checkin.createdAt ?? "n/a")
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    Divider()
-                }
-            }
-        }
-        .padding(32.0)
-        .onAppear {
-            print("checkin appear")
-            checkinsViewModel.setupActionCable()
-            checkinsViewModel.socket?.delegate = self
-        }
-        .onDisappear {
-            checkinsViewModel.socket?.delegate = nil
-        }
-
-    }
-}
 
 struct DashboardView: View {
     @Environment(\.presentationMode) var presentationMode
-
     @State private var isActive: Bool = false
     @ObservedObject var merchantViewModel = MerchantViewModel()
-    
-    let checkinsListView = CheckinsListView()
+    @ObservedObject var checkinsViewModel = CheckinsViewModel()
+
+    @State var selectedUserCheckin: Checkin?
 
     var body: some View {
         NavigationView {
         VStack {
+            Text(merchantViewModel.merchant.legalName ?? "")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+                .padding()
             HStack {
-                if #available(iOS 15.0, *) {
-                    Image("ic_person")
-                        .scaledToFill()
-                        .clipShape(Circle())
-                        .frame(width: 100, height: 100, alignment: .center)
-                        .overlay {
-                            Circle().stroke(.gray, lineWidth: 4)
-                        }
-                        .shadow(radius: 7.0)
-                        .padding()
-                } else {
-                        Image(uiImage: .checkmark)
-                            .clipShape(Circle())
-                            .frame(width: 100, height: 100, alignment: .leading)
-                            .shadow(radius: 7.0)
-                            .padding()
+                if (selectedUserCheckin != nil) {
+                    CheckinUserInfoView(username: selectedUserCheckin?.userHandle ?? "n/a", timestamp: selectedUserCheckin?.createdAt ?? "n/a")
                 }
-                
-                VerifiedAnimation()
                 Spacer()
-                Text(merchantViewModel.merchant.legalName ?? "")
-                    .fontWeight(.semibold)
-                    .padding()
-                
                 if (merchantViewModel.merchantUserHandle != "" && merchantViewModel.merchantUserHandle != nil) {
                     QRCodeView(QRString: QRCodeEventType.checkin(merchantUserHandle: merchantViewModel.merchantUserHandle ?? "") ?? "")
                 }
             }
             .padding()
             Spacer(minLength: 16)
-            checkinsListView
+            CheckinsListView(checkins: $checkinsViewModel.checkins, selectedUserCheckin: $selectedUserCheckin)
         }
         .navigationTitle(!isActive ? "VEEV" : "")
         .navigationBarHidden(false)
@@ -113,21 +44,19 @@ struct DashboardView: View {
             NavigationLink("Settings", destination: SettingsView(), isActive: self.$isActive)
         }
         .onAppear(perform: {
-            print("dashboard appear")
+            checkinsViewModel.setupActionCable()
+            checkinsViewModel.socket?.delegate = self
         })
-        .onDisappear {
-            print("dashboard disappeared")
-        }}
+        }
         .navigationViewStyle(.stack)
     }
 }
 
-extension CheckinsListView: ActionCableDelegate {
+extension DashboardView: ActionCableDelegate {
     func cable(_ actionCable: ActionCable, response data: Data) {
         do {
             let decoded = try actionCable.decoder.decode(CheckInPushData.self, from: data)
             if let _ = decoded.type { return }
-            
             self.checkinsViewModel.serializeData(data: data)
         } catch {
             print("error decoding json =>", error)
