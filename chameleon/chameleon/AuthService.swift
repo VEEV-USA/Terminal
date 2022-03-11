e353ff20a4d567abcfe437a315a0e968d7270266
hades <hadeez.dare@gmail.com>
2022-03-11 15:52:49 -0800

    keep session if previously logged in

--- chameleon/chameleon/AuthService.swift
+++ chameleon/chameleon/AuthService.swift
@@ -6,7 +6,21 @@
 //
 
 import SwiftUI
+import CoreData
 
 class AuthService: ObservableObject {
     @Published var isAuth: Bool = false
+    
+    
+    init() {
+        let fetchRequest: NSFetchRequest<User> = NSFetchRequest(entityName: "User")
+        fetchRequest.fetchLimit = 1
+        do {
+            if let _ = try Persistence.sharedManager.container.viewContext.fetch(fetchRequest).first {
+                isAuth.toggle()
+            }
+        } catch {
+            print("cannot fetch user ", error.localizedDescription)
+        }
+    }
 }
